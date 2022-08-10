e51c053608121151ad3573492a46225c2bfb82c4
ArmandoFigueiredo <106556517+ArmandoFigueiredo@users.noreply.github.com>
2022-08-10 19:26:42 -0300

    done

--- js/index.js
+++ js/index.js
@@ -4,3 +4,44 @@
 
 
 // Iteration 3: Loops
+
+const driverc = "hacker1";
+const chrome = "hacker2";
+
+console.log(`the driver's name is ${driverc} `);
+console.log(`the navigator's name is ${chrome}`);
+
+if (driverc.length > chrome.length){
+    console.log(`The driver has the longest name, it has ${driverc.length} characters.`)
+} else if (chrome.length > driverc.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${chrome.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${driverc.length} characters!`)
+}
+
+const driver = "j h o n"
+console.log(driver.toUpperCase());
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
